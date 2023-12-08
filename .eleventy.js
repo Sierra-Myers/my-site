64104f51052079f9369dce27f0309d8c41876402
Sierra-Myers <53084343+Sierra-Myers@users.noreply.github.com>
2023-12-07 21:22:19 -0500

    json edit

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/*");
     eleventyConfig.addPassthroughCopy("images/*");
     eleventyConfig.addPassthroughCopy("scripts/*");
+    
+    return { 
+      passthroughFileCopy:true,
+        dir:{
+          input: "src",
+          output: "_site",
+          include: "include"
+        }
+    }
   };
